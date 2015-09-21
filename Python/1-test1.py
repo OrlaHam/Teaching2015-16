0a420d0d6278494d55799167f31499e4430fe249
Mark Foley <mark@markfoley.info>
2015-09-21 09:38:06 +0100

    initial commit - new directory

---
+++ Python/1-test1.py
@@ -0,0 +1,3 @@
+__author__ = 'mark'
+
+print("Hello World")
---
+++ Python/4-turtle_star.py
@@ -0,0 +1,14 @@
+__author__ = 'mark'
+
+import turtle
+
+turtle.forward(100)
+turtle.right(144)
+turtle.forward(100)
+turtle.right(144)
+turtle.forward(100)
+turtle.right(144)
+turtle.forward(100)
+turtle.right(144)
+turtle.forward(100)
+turtle.right(144)
