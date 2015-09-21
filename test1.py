cdcacb69f1b0146c0dbc556aa8b73a38732bffb7
Mark Foley <mark@markfoley.info>
2015-09-21 08:54:51 +0100

    initial commit

---
+++ test1.py
@@ -0,0 +1,24 @@
+__author__ = 'mark'
+
+
+a = input("Enter a: ")
+b = input("Enter b: ")
+
+if a == b:
+    print("Equal!")
+else:
+    print("not equal")
+
+if a == b:
+    # do stuff
+    print("stuff")
+    print("more stuff")
+else:
+    print("yet more stuff")
+
+print(2+2)
+
+if a == b:
+    if x == y:
+        print()
+
