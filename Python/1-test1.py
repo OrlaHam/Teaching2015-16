5a99db4fe52f3c55357f068599c3947ed62f9afc
Mark Foley <mark@markfoley.info>
2015-10-09 09:49:21 +0100

    Latest exercise solutions and example programs

--- Python/1-test1.py
+++ Python/1-test1.py
@@ -1,3 +1,18 @@
 __author__ = 'mark'
 
 print("Hello World")
+
+a_very_long_variable_name = 42
+
+print(a_very_long_variable_name)
+
+a_piece_of_text = "random stuff"
+
+a_piece_of_text.capitalize()
+
+an_int = 22
+
+import math
+
+math.pi
+
--- Python/int2bin.py
+++ Python/int2bin.py
@@ -8,41 +8,27 @@ print('Int to binary')
 try:
     int_str = input('Give me an int: ')
     myInt = int(int_str)
+
+    bin_str = ''
+    while myInt > 0:
+        bin_str = str(myInt % 2) + bin_str
+        myInt //= 2
+
+    print('The binary of', int_str, 'is', bin_str)
+
+    print('\nBinary to int')
+    bin_str = input('Give me a binary string: ')
+
+    temp = bin_str
+    new_int = 0
+    power = 0
+    while len(temp) > 0:
+        bit = int(temp[-1])
+        new_int = new_int + bit * 2 ** power
+        temp = temp[:-1]
+        power += 1
+
+    print(bin_str, 'to integer is', new_int)
+
 except ValueError as e:
     print("You big eejit! Try again")
-
-bin_str = ''
-while myInt > 0:
-    bin_str = str(myInt % 2) + bin_str
-    myInt //= 2
-
-print('The binary of', int_str, 'is', bin_str)
-
-print('\nBinary to int')
-bin_str = input('Give me a binary string: ')
-
-temp = bin_str
-new_int = 0
-power = 0
-while len(temp) > 0:
-    bit = int(temp[-1])
-    new_int = new_int + bit * 2 ** power
-    temp = temp[:-1]
-    power += 1
-
-print(bin_str, 'to integer is', new_int)
-
-
-# something = "stuff"
-# if something=="something":
-#     # do stuff
-#     pass
-# else:
-#     # do other stuff
-#     pass
-#
-#
-# if (i == 1) {
-#
-# }
-#
